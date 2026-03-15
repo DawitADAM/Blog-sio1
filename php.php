8edb0065592d869d9c7057cd437db77f82fefa8e
Subnau940 <grechmatteo68@gmail.com>
2026-03-16 00:46:25 +0100

    Update php.php

--- php.php
+++ php.php
@@ -3,9 +3,10 @@ $host    = getenv('MYSQLHOST');
 $db      = getenv('MYSQLDATABASE');
 $user    = getenv('MYSQLUSER');
 $pass    = getenv('MYSQLPASSWORD');
+$port    = getenv('MYSQLPORT') ?: '3306';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
